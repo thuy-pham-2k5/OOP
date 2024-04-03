e730045d452ecb7f43af56588b0c0ff6e800928e
Thuy Pham <phamthikimthuy05@gmail.com>
2024-04-03 16:26:56 +0700

    Btvn 3 phần trước ạ, em làm nốt bài cuối tối nay nộp sau ạ.

---
+++ src/num_into_word.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class num_into_word {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int number;
+        System.out.println("Nhập số bạn muốn đọc: ");
+        number = scanner.nextInt();
+        int hundred;
+
+    }
+}
